d06d35a56989e10bb202cae244b0f8feb029c046
simennb <simen.bastnes@gmail.com>
2016-11-05 16:49:06 +0100

    totoro

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+#include <algorithm>
+#include <cmath>
+#include <armadillo>
+#include <time.h>
+
+using namespace std;
+using namespace arma;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
