2ca80b233118cbb56e136bcaf6d384042a10bcbe
simennb <simen.bastnes@gmail.com>
2016-11-13 17:56:09 +0100

    fixing writetofile

--- src/ising.h
+++ src/ising.h
@@ -7,7 +7,7 @@
 
 void InitializeLattice(int L, arma::mat &SpinMatrix, double &E, double &M, std::string direction);
 
-void Metropolis(int L, int MCcycles, double T, arma::vec &ExpectationValues, const char *dir, std::string filename);
+void Metropolis(int L, int MCcycles, double T, arma::vec &ExpectationValues, const char *dir, std::ofstream &m_file);
 
 // Function for periodic boundary conditions
 inline int periodic(int i, int limit, int add){
